8054a5f91d38261b75bba77f2bc625254c63884e
theirongiant <186162+theirongiant@users.noreply.github.com>
2018-08-21 06:10:23 +0100

    v0.0.1
    First working version

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 import React, { Component, Fragment } from "react";
 import convert from "color-convert";
-
-console.log(convert.hex.hsl("ff2456"));
+import randomcolor from "randomcolor";
 
 const rgbSimple = /^#?[0-9,a-f,A-F]{3}|[0-9,a-f,A-F]{6}$/;
 const hueTable = {
   red: convert.keyword.hsl("red")[0],
-  orange: convert.keyword.hsl("blue")[0],
+  orange: convert.keyword.hsl("orange")[0],
   yellow: convert.keyword.hsl("yellow")[0],
   green: convert.keyword.hsl("green")[0],
   blue: convert.keyword.hsl("blue")[0],
   indigo: convert.keyword.hsl("indigo")[0],
   violet: convert.keyword.hsl("violet")[0]
 };
-
-console.log(hueTable);
+const startColor = randomcolor();
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      lastGood: "000",
-      lastGoodHSL: convert.hex.hsl("000"),
-      baseColor: "#000",
+      lastGood: startColor,
+      lastGoodHSL: convert.hex.hsl(startColor),
+      baseColor: startColor,
       validColor: true
     };
 
@@ -31,6 +29,7 @@ class App extends Component {
   }
 
   render() {
+    const { lastGoodHSL } = this.state;
     return (
       <Fragment>
         <header>
@@ -46,7 +45,7 @@ class App extends Component {
             <div
               className="color-box"
               style={{
-                backgroundColor: "#" + this.state.lastGood,
+                backgroundColor: this.state.lastGood,
                 margin: "5px auto"
               }}
             />
@@ -56,20 +55,57 @@ class App extends Component {
               onChange={e => this.handleBaseColor(e.target.value)}
               size={7}
             />
+            <button
+              onClick={() => {
+                this.handleBaseColor(randomcolor());
+              }}
+            >
+              Random
+            </button>
           </div>
           <div />
         </section>
 
         <section className="container">
-          {Object.entries(this.state.rainbow).map(([color, rgb]) => {
-            return (
-              <div
-                key={color}
-                className="color-box"
-                style={{ backgroundColor: `#${rgb}` }}
-              />
-            );
-          })}
+          {Object.entries(this.generateRainbow(lastGoodHSL, 15)).map(
+            ([color, rgb]) => {
+              return (
+                <div
+                  key={color}
+                  className="color-box"
+                  style={{ backgroundColor: `#${rgb}` }}
+                />
+              );
+            }
+          )}
+        </section>
+
+        <section className="container">
+          {Object.entries(this.generateRainbow(lastGoodHSL)).map(
+            ([color, rgb]) => {
+              return (
+                <div
+                  key={color}
+                  className="color-box"
+                  style={{ backgroundColor: `#${rgb}` }}
+                />
+              );
+            }
+          )}
+        </section>
+
+        <section className="container">
+          {Object.entries(this.generateRainbow(lastGoodHSL, -15)).map(
+            ([color, rgb]) => {
+              return (
+                <div
+                  key={color}
+                  className="color-box"
+                  style={{ backgroundColor: `#${rgb}` }}
+                />
+              );
+            }
+          )}
         </section>
 
         <footer />
@@ -84,8 +120,7 @@ class App extends Component {
       this.setState({
         lastGood: good,
         baseColor: value,
-        lastGoodHSL: hsl,
-        rainbow: this.generateRainbow(hsl)
+        lastGoodHSL: hsl
       });
     } else {
       this.setState({
@@ -94,7 +129,7 @@ class App extends Component {
     }
   }
 
-  generateRainbow(base) {
+  generateRainbow(base, distance = 0) {
     return [
       "red",
       "orange",
@@ -104,7 +139,11 @@ class App extends Component {
       "indigo",
       "violet"
     ].reduce((obj, color) => {
-      obj[color] = convert.hsl.hex(hueTable[color], base[1], base[2]);
+      obj[color] = convert.hsl.hex(
+        hueTable[color] + distance,
+        base[1],
+        base[2]
+      );
       return obj;
     }, {});
   }
