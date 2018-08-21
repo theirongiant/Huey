b8fb0dd90c9f0edd6ee1c5046c7887e5d1494e10
theirongiant <186162+theirongiant@users.noreply.github.com>
2018-08-21 04:52:09 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Huey
-Created with CodeSandbox
---
+++ src/app.js
@@ -0,0 +1,113 @@
+import React, { Component, Fragment } from "react";
+import convert from "color-convert";
+
+console.log(convert.hex.hsl("ff2456"));
+
+const rgbSimple = /^#?[0-9,a-f,A-F]{3}|[0-9,a-f,A-F]{6}$/;
+const hueTable = {
+  red: convert.keyword.hsl("red")[0],
+  orange: convert.keyword.hsl("blue")[0],
+  yellow: convert.keyword.hsl("yellow")[0],
+  green: convert.keyword.hsl("green")[0],
+  blue: convert.keyword.hsl("blue")[0],
+  indigo: convert.keyword.hsl("indigo")[0],
+  violet: convert.keyword.hsl("violet")[0]
+};
+
+console.log(hueTable);
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lastGood: "000",
+      lastGoodHSL: convert.hex.hsl("000"),
+      baseColor: "#000",
+      validColor: true
+    };
+
+    this.state.rainbow = this.generateRainbow(this.state.lastGoodHSL);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <header>
+          <h1>Huey</h1>
+          <p>
+            Enter a colour and get a range of hues with the same saturation and
+            lightness
+          </p>
+        </header>
+
+        <section>
+          <div className="base-box">
+            <div
+              className="color-box"
+              style={{
+                backgroundColor: "#" + this.state.lastGood,
+                margin: "5px auto"
+              }}
+            />
+            <input
+              type="text"
+              value={this.state.baseColor}
+              onChange={e => this.handleBaseColor(e.target.value)}
+              size={7}
+            />
+          </div>
+          <div />
+        </section>
+
+        <section className="container">
+          {Object.entries(this.state.rainbow).map(([color, rgb]) => {
+            return (
+              <div
+                key={color}
+                className="color-box"
+                style={{ backgroundColor: `#${rgb}` }}
+              />
+            );
+          })}
+        </section>
+
+        <footer />
+      </Fragment>
+    );
+  }
+
+  handleBaseColor(value) {
+    if (rgbSimple.test(value)) {
+      const good = value.replace("#", "");
+      const hsl = convert.hex.hsl(good);
+      this.setState({
+        lastGood: good,
+        baseColor: value,
+        lastGoodHSL: hsl,
+        rainbow: this.generateRainbow(hsl)
+      });
+    } else {
+      this.setState({
+        baseColor: value
+      });
+    }
+  }
+
+  generateRainbow(base) {
+    return [
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      "blue",
+      "indigo",
+      "violet"
+    ].reduce((obj, color) => {
+      obj[color] = convert.hsl.hex(hueTable[color], base[1], base[2]);
+      return obj;
+    }, {});
+  }
+}
+
+export default App;
